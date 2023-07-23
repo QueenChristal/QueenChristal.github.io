a42a6c92ca3ab8c31d7a95ad9988c2d6052457d1
Oghogho Christiana Ejowoh <ecchristiana@gmail.com>
2023-07-23 23:43:18 +0100

    Add files via upload

---
+++ display.js
@@ -0,0 +1,36 @@
+
+const container = document.getElementById("container")
+const msg = document.getElementById("msg")
+const msgBtn = document.getElementById("msg-btn")
+
+let displayCss = `
+ <video autoplay muted id="myVideo">
+  <source src="../videos/happyBirthday.mp4" type="video/mp4">
+ </video>
+ <style>
+ body{
+   background-color: black;
+   background-repeat: no-repeat;
+   background-position: top center;
+   position: absolute;
+  background-size: cover;
+  position: absolute;
+  overflow: hidden;
+  color: white;
+ }
+ #myVideo{
+   height: 720px;
+   width: 360px;
+ }
+ </style>
+ `
+
+msgBtn.addEventListener("click", function(){
+   container.innerHTML = displayCss 
+   playSound()
+})
+
+function playSound(){
+  let ding = new Audio('../music/happyBirthday.mp3')
+  ding.play()
+}
